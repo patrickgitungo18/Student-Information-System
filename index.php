8f591d0b5fd92b4ca4310087be02158ca230bf61
Patrick Jan Gitungo <100455454+patrickgitungo18@users.noreply.github.com>
2023-09-09 08:08:59 +0800

    Putting nav-menu and Styling
    Creating navigation menu and style it in CSS

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Student Information System</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <h1>Student Informatiion <br>System</h1>
+        <ul>
+            <li><a href="index.php">Dashboard</a></li>
+            <li><a href="">Add Student</a></li>
+            <li><a href="">Logout</a></li>
+        </ul>
+
+        
+    </body>
+</html>
